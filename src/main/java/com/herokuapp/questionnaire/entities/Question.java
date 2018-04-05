f2cbea746ae98b56565e32628857f4df036fbabd
Fodor József Felicián <fodorj@jusoft.hu>
2018-04-05 14:47:41 +0200

    first commit

---
+++ src/main/java/com/herokuapp/questionnaire/entities/Question.java
@@ -0,0 +1,16 @@
+package com.herokuapp.questionnaire.entities;
+
+import javax.persistence.Entity;
+
+
+public class Question {
+	
+	private Long id;
+	
+	private String topic;
+	private String title;
+	private String question;
+	
+	private int answer;
+
+}
