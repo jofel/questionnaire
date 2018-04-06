228902edaf588238341e14163f9ba106f77a2082
Fodor József Felicián <fodorj@jusoft.hu>
2018-04-07 00:17:27 +0200

    Menu element fixed and template questionnaire page created

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,18 +1,43 @@
-new Vue({
-	el: '#vue-app',
-	data: {
-		topics: ['Általános', 'Kultura', 'Sport', 'Higénia'],
-		questions: [
-			{ topic: 'Általános', title: 'Általános 01', question:'Mit gondol általában a ...-ról?', answer:'0'},
-			{ topic: 'Kultura', title: 'Kultura 01', question:'Mit gondol a Kulturá-ról?', answer:'0'},
-			{ topic: 'Sport', title: 'Sport 01', question:'Mit gondol a Sport-ról?', answer:'0'},
-			{ topic: 'Higénia', title: 'Higénia 01', question:'Mit gondol a Higénia-ról?', answer:'0'}
-		],
-		name: 'Jofel',
-	},
-	methods: {
-		great: function(time){
-			return 'Jó ' + time + ' ' + this.name;
-		}
-	}
-})
+new Vue(
+		{
+			el : '#vue-app',
+			data : {
+				topics : [ 'Általános', 'Kultura', 'Sport', 'Higénia' ],
+				questions : [
+						{
+							topic : 'Általános',
+							title : 'Általános 01',
+							question : 'Mit gondol általában a ..........................................................-ról?',
+							answer : '0'
+						},
+						{
+							topic : 'Kultura',
+							title : 'Kultura 01',
+							question : 'Mit gondol a Kulturá-ról?',
+							answer : '0'
+						},
+						{
+							topic : 'Sport',
+							title : 'Sport 01',
+							question : 'Mit gondol a Sport-ról?',
+							answer : '0'
+						},
+						{
+							topic : 'Higénia',
+							title : 'Higénia 01',
+							question : 'Mit gondol általában a .................. ................... .......... ............... ............ ...........................-ról?',
+							answer : '0'
+						} ],
+				name : 'Jofel',
+			},
+			methods : {
+				great : function(time) {
+					return 'Jó ' + time + ' ' + this.name;
+				}
+			}
+		})
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
