37a787dea856ac087693eec337780880ebba7c5f
Fodor József Felicián <fodorj@jusoft.hu>
2018-04-14 12:22:55 +0200

    questions panels and answers pic modified

--- src/main/java/com/herokuapp/questionnaire/services/InitApplicationService.java
+++ src/main/java/com/herokuapp/questionnaire/services/InitApplicationService.java
@@ -21,26 +21,22 @@ public class InitApplicationService {
 	public void initializeTestData() {
 		LOGGER.info("Initialize test data");
 
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
-		questionService.saveQuestion(new Question("Topic 1", "title 1",
-				"question 1 question 1 question 1 question 1question 1question 1", 1));
+		questionService
+				.saveQuestion(new Question("Topic 1", "title 1", "A kollégium környezete biztonságos, védett.", 1));
+		questionService.saveQuestion(new Question("Topic 1", "title 1", "Kollégiumi könyvtárt hasznosnak találom.", 1));
+		questionService
+				.saveQuestion(new Question("Topic 1", "title 1", "A WiFi hálózatra szükségem van a szobában is.", 1));
+		questionService.saveQuestion(new Question("Topic 1", "title 1", "A zuhanyzók állapota megfelelő", 1));
+		questionService.saveQuestion(
+				new Question("Topic 1", "title 1", "Megfelelő mennyiségű sportkör van a kollégiumban", 1));
+		questionService.saveQuestion(
+				new Question("Topic 1", "title 1", "Nem zavar, hogy nincs világítás este a folyosón.", 1));
+		questionService
+				.saveQuestion(new Question("Topic 1", "title 1", "Nem zavar, hogy a lift nem üzemel éjszaka.", 1));
+		questionService.saveQuestion(new Question("Topic 1", "title 1", "Nincs hideg a szobámban télen sem.", 1));
+		questionService.saveQuestion(new Question("Topic 1", "title 1", "Szoba bútorzata elfogadható.", 1));
+		questionService.saveQuestion(
+				new Question("Topic 1", "title 1", "Elégedett vagyok a Kollégium Bizottság munkájával.", 1));
 
 		LOGGER.info("Initialization completed");
 	}
