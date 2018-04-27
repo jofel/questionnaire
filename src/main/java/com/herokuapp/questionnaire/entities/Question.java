60a0a6bdc57c41bd3fef1f67c699f26abde9ab4a
Fodor József Felicián <fodor.jofel@gmail.com>
2018-04-27 11:24:12 +0200

    question entity modified

--- src/main/java/com/herokuapp/questionnaire/entities/Question.java
+++ src/main/java/com/herokuapp/questionnaire/entities/Question.java
@@ -16,26 +16,14 @@ public class Question {
 	@Column(name = "id")
 	private Long id;
 
-	@Column(name = "topic")
-	private String topic;
-
-	@Column(name = "title")
-	private String title;
-
 	@Column(name = "question")
 	private String question;
-
-	@Column(name = "answer")
-	private int answer;
 
 	public Question() {
 	}
 
-	public Question(String topic, String title, String question, int answer) {
-		this.topic = topic;
-		this.title = title;
+	public Question(String question) {
 		this.question = question;
-		this.answer = answer;
 	}
 
 	public Long getId() {
@@ -46,36 +34,12 @@ public class Question {
 		this.id = id;
 	}
 
-	public String getTopic() {
-		return topic;
-	}
-
-	public void setTopic(String topic) {
-		this.topic = topic;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 	public String getQuestion() {
 		return question;
 	}
 
 	public void setQuestion(String question) {
 		this.question = question;
-	}
-
-	public int getAnswer() {
-		return answer;
-	}
-
-	public void setAnswer(int answer) {
-		this.answer = answer;
 	}
 
 }
