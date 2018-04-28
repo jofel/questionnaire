f80f78eb72f992b2cba1d091d192b42e5882f04d
Fodor József Felicián <fodor.jofel@gmail.com>
2018-04-28 17:53:49 +0200

    changed id type to String

--- src/main/java/com/herokuapp/questionnaire/entities/Question.java
+++ src/main/java/com/herokuapp/questionnaire/entities/Question.java
@@ -1,22 +1,22 @@
 package com.herokuapp.questionnaire.entities;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
 @Entity
 @Table(name = "question_table")
 public class Question {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id")
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid2")
+	private String id;
 
-	@Column(name = "question")
 	private String question;
 
 	public Question() {
@@ -26,11 +26,11 @@ public class Question {
 		this.question = question;
 	}
 
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
